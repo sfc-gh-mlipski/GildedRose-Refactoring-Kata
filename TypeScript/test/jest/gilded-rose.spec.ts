f06e06852d82931d8592aea654b7e071e53f693e
Michał Lipski <michal.lipski@snowflake.com>
2022-09-26 14:14:24 +0200

    black box tests

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -2,8 +2,57 @@ import { Item, GildedRose } from '@/gilded-rose';
 
 describe('Gilded Rose', () => {
   it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
+    const gildedRose = new GildedRose([ new Item("+5 Dexterity Vest", 10, 20), //
+      new Item("Aged Brie", 2, 0),
+      new Item("Elixir of the Mongoose", 5, 7),
+      new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+      new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49)]);
+
+    gildedRose.updateQuality();
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('fixme');
+
+    expect(items).toEqual([{
+      "name": "+5 Dexterity Vest",
+      "quality": 18,
+      "sellIn": 8
+    },
+      {
+        "name": "Aged Brie",
+        "quality": 2,
+        "sellIn": 0
+      },
+      {
+        "name": "Elixir of the Mongoose",
+        "quality": 5,
+        "sellIn": 3
+      },
+      {
+        "name": "Sulfuras, Hand of Ragnaros",
+        "quality": 80,
+        "sellIn": 0
+      },
+      {
+        "name": "Sulfuras, Hand of Ragnaros",
+        "quality": 80,
+        "sellIn": -1
+      },
+      {
+        "name": "Backstage passes to a TAFKAL80ETC concert",
+        "quality": 22,
+        "sellIn": 13
+      },
+      {
+        "name": "Backstage passes to a TAFKAL80ETC concert",
+        "quality": 50,
+        "sellIn": 8
+      },
+      {
+        "name": "Backstage passes to a TAFKAL80ETC concert",
+        "quality": 50,
+        "sellIn": 3
+      }])
   });
 });
